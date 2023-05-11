6daa484a47e10d41262cc36cc594225ed590d8a5
saeraphas <saeraphas@users.noreply.github.com>
2023-05-11 16:57:05 -0400

    update
    remove embedded SKU translation keypair array
    add SKU translation download from MS docs with bypass
    rewrite user attribute collection to use MGGraph instead of MSOL
    change report column order

--- Download-MicrosoftLicenseInformation.ps1
+++ Download-MicrosoftLicenseInformation.ps1
@@ -1,9 +1,9 @@
 #product names and service 
 #https://learn.microsoft.com/en-us/azure/active-directory/enterprise-users/licensing-service-plan-reference
 
-$MicrosoftDocumentationURI = "https://learn.microsoft.com/en-us/azure/active-directory/enterprise-users/licensing-service-plan-reference"
-$MicrosoftDocumentationCSVURI = ((Invoke-WebRequest -UseBasicParsing –Uri $MicrosoftDocumentationURI).Links | Where-Object { $_.href -like "http*" } | Where-Object { $_.href -like "*.csv" }).href
-$MicrosoftProducts = Invoke-RestMethod -Uri $MicrosoftDocumentationCSVURI -Method Get | ConvertFrom-CSV
-$MicrosoftProducts | Out-GridView
-#$MicrosoftProductsHashTable = @{}
-#Foreach ($Sku in $MicrosoftProducts) { $MicrosoftProductsHashTable.Add([string]$Sku.String_Id, [string]$Sku.Product_Display_Name) }
+#$MicrosoftDocumentationURI = 'https://learn.microsoft.com/en-us/azure/active-directory/enterprise-users/licensing-service-plan-reference'
+#$MicrosoftDocumentationCSVURI = ((Invoke-WebRequest -UseBasicParsing -Uri $MicrosoftDocumentationURI).Links | Where-Object { $_.href -like 'http*' } | Where-Object { $_.href -like '*.csv' }).href
+#$MicrosoftProducts = Invoke-RestMethod -Uri $MicrosoftDocumentationCSVURI -Method Get | ConvertFrom-CSV | Select-Object String_ID, Product_Display_Name -Unique
+
+#$SearchSKU = 'ENTERPRISEPACK'
+#$MicrosoftProducts | Where-Object { $_.String_ID -eq $SearchSKU } | Select-Object Product_Display_Name
